84986f204aa8377265ea0ed97ce71fb4118eebb2
Maman Ale <maman.darman1986@gmail.com>
2025-10-04 23:40:52 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -154,7 +154,7 @@ import streamlit as st
 st.set_page_config(page_title="Fiber Optic Analyzer", layout="wide")
 
 # Menampilkan logo dan judul
-col1, col2 = st.columns([1, 5])
+col1, col2 = st.columns([2, 5])
 with col1:
     st.image("HWI.png", width=50)  # logo
 with col2:
