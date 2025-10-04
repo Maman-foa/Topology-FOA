0b748dbd5a77c05c5509cbcfb3279856c72ba744
Maman Ale <maman.darman1986@gmail.com>
2025-10-04 22:48:29 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -61,11 +61,8 @@ import uuid
 
 def get_device_info():
     hostname = socket.gethostname()
-    try:
-        mac_addr = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
-                             for ele in range(0, 8*6, 8)][::-1])
-    except Exception as e:
-        mac_addr = "unknown_mac"
+    mac_addr = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
+                         for ele in range(0,8*6,8)][::-1])
     return mac_addr, hostname
 
 # ======================
